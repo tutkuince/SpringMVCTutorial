10f3fc1cd8d058cf3a52bba8dee70c86c35f804d
tutkuince <tutku.ince@outlook.com>
2018-05-16 14:18:05 +0300

    @MatrixVariable param

--- src/io/spring/part02/_02controller/ProductController.java
+++ src/io/spring/part02/_02controller/ProductController.java
@@ -1,7 +1,10 @@
 package io.spring.part02._02controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,5 +26,14 @@ public class ProductController {
 	public String productDetail(@RequestParam String productId, Model model) {
 		model.addAttribute("message", "productId: " + productId);
 		return "_02productDetail";
+	}
+	
+	@RequestMapping("/product.moredetail/{matrixVariables}")
+	public String moreDetailParameters(
+			@MatrixVariable(pathVar="matrixVariables", name="color") List<String> colors,
+			@MatrixVariable(pathVar="matrixVariables", name="lowPrice") String lowPrice,
+			@MatrixVariable(pathVar="matrixVariables", name="highPrice") String highPrice, Model model) {
+		model.addAttribute("message", "moreDetailParameters : " + lowPrice + " " + highPrice + " " + colors);
+		return "_02extradetailedProduct";
 	}
 }
