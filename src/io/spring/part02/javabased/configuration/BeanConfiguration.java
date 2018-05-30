c44718a90ac15c1a0197ecde941a60b94f87ffa5
tutkuince <tutku.ince@outlook.com>
2018-05-30 13:26:54 +0300

    codes commented in registerSpring and BeanConfiguration classes for prevent java based configuration

--- src/io/spring/part02/javabased/configuration/BeanConfiguration.java
+++ src/io/spring/part02/javabased/configuration/BeanConfiguration.java
@@ -1,25 +1,25 @@
 package io.spring.part02.javabased.configuration;
 
-import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+//import org.springframework.context.annotation.Bean;
+//// import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.ViewResolver;
+//import org.springframework.web.servlet.view.InternalResourceViewResolver;
+//
+// import io.spring.part02.javabased.controller.PersonController;
 
- import io.spring.part02.javabased.controller.PersonController;
-
-@Configuration
+//@Configuration
 //@ComponentScan("io.spring.part02.javabased")
 public class BeanConfiguration {
 
-	 @Bean
-	 public PersonController createPersonController() {
-	 return new PersonController();
-	 }
-
-	@Bean
-	public ViewResolver createViewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver("/WEB-INF/view/", ".jsp");
-		return viewResolver;
-	}
+//	 @Bean
+//	 public PersonController createPersonController() {
+//	 return new PersonController();
+//	 }
+//
+//	@Bean
+//	public ViewResolver createViewResolver() {
+//		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver("/WEB-INF/view/", ".jsp");
+//		return viewResolver;
+//	}
 }
